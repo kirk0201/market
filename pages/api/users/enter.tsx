621ce4e01d96e4cdf80b5a00fdaf91caa3d0e318
DESKTOP-NFT79S5\mine6 <mine6546@gmail.com>
2022-05-19 23:46:14 +0900

    Feat: SendGrid 메일 인증 추가 - 로그인 제한됨 (회사 방침 문제) - 로그인 후 key 값 환경변수 넣을 예정

--- pages/api/users/enter.tsx
+++ pages/api/users/enter.tsx
@@ -1,23 +1,43 @@
+import twilio from "twilio";
 import client from "@libs/server/client";
-import withHandler from "@libs/server/withHandler";
+import withHandler, { ResponseType } from "@libs/server/withHandler";
 import { NextApiRequest, NextApiResponse } from "next";
 
-async function handler(req: NextApiRequest, res: NextApiResponse) {
+const twilioClient = twilio(process.env.TWILIO_SID, process.env.TWILIO_TOKEN);
+async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<ResponseType>
+) {
   const { email, phone } = req.body;
-  const payload = email ? { email } : { phone: +phone };
-  const user = await client({
-    where: {
-      ...payload,
+  const user = email ? { email } : phone ? { phone: +phone } : null;
+  if (!user) return res.status(400).json({ ok: false });
+  const random = Math.floor(100000 + Math.random() * 9000000) + "";
+  const token = await client.token.create({
+    data: {
+      token: random,
+      user: {
+        connectOrCreate: {
+          where: {
+            ...user,
+          },
+          create: {
+            name: "Annoymous",
+            ...user,
+          },
+        },
+      },
     },
-    create: {
-      name: "Annoymous",
-      ...payload,
-    },
-    update: {},
   });
-  console.log(user);
-
-  return res.status(200).end();
+  console.log(token);
+  if (phone) {
+    await twilioClient.messages.create({
+      messagingServiceSid: process.env.TWILIO_MSID,
+      // to: 수신자 번호로 input에서 받은 phone을 해야하지만 Trial계정으로 제한되기에 내 폰번호로 지정
+      to: process.env.MY_PHONE!,
+      body: `Your login token is ${random}`,
+    });
+  }
+  return res.json({ ok: true });
 }
 
 export default withHandler("POST", handler);
